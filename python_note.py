82bce9631a1cb1cc0a3de847c5322cdd5a51357a
BigBrotherWatchingYou <99124106+BigBrotherWatchingYou@users.noreply.github.com>
2024-01-30 22:08:00 +0800

    00

---
+++ python_note.py
@@ -0,0 +1,7 @@
+"""which of this create a car?"""
+class car(object):
+    def _init_(self,w,d):
+        self.wheels = w
+        self.doors = d
+mycar = car(mycar,2,4)
+# this one in wrong: car(mycar,4,2)

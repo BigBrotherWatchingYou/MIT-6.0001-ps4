784f2776a017f24587a6081e0b27607980ee45a7
BigBrotherWatchingYou <99124106+BigBrotherWatchingYou@users.noreply.github.com>
2024-02-04 11:18:18 +0800

    0

--- python_note.py
+++ python_note.py
@@ -5,3 +5,21 @@ class car(object):
         self.doors = d
 mycar = car(mycar,2,4)
 # this one in wrong: car(mycar,4,2)     car is just like a "kx+b", and mycar= y----y=kx+b
+
+# getters and setters
+class Animal(object):
+    def _init_(self, age):
+        self.age = age
+        self.name = None
+    def get_age(self):
+        #tell the age
+        return self.age
+    def set_age(self, newage):
+        #reset the age
+        self.age = newage
+    def set_name(self, newname=""):
+        #set name
+        self.name = newname
+    def _str_(self):
+        #turn it into str
+        return "animal:"+str(self.name)+":"+str(self.age)

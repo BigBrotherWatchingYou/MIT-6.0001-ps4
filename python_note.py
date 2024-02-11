886b8ca5a05a3530af4c2ee25bef6adfd023d93e
BigBrotherWatchingYou <99124106+BigBrotherWatchingYou@users.noreply.github.com>
2024-02-11 16:10:46 +0800

    0000

--- python_note.py
+++ python_note.py
@@ -1,9 +1,4 @@
-"""which of this create a car?"""
-class car(object):
-    def _init_(self,w,d):
-        self.wheels = w
-        self.doors = d
-mycar = car(mycar,2,4)
+
 # this one in wrong: car(mycar,4,2)     car is just like a "kx+b", and mycar= y----y=kx+b
 
 # getters and setters
@@ -13,6 +8,7 @@ class Animal(object):
         self.name = None
     def get_age(self):
         #tell the age
+        # Remember there should be a (self)
         return self.age
     def set_age(self, newage):
         #reset the age
@@ -20,11 +16,19 @@ class Animal(object):
     def set_name(self, newname=""):
         #set name
         self.name = newname
+   
     def _str_(self):
         #turn it into str
         return "animal:"+str(self.name)+"age:"+str(self.age)
     
     # how it works:  
     a = Animal(lion)
-    a.get_age(55)
+    a.set_age(55)
     
+    # Inherits from Animal
+    class Dog(Animal):
+        def speak(self):
+          print("Ruff Ruff")
+    d= Dog(7)
+    d.set_name("Ruffles")
+    d.speak()
